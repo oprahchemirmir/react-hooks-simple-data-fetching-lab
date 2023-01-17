3dad21ba639e5d03d0272e2f14b467576d4302a0
oprah <oprah.chemirmir@student.moringaschool.com>
2023-01-17 06:36:09 +0300

    done with assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,19 @@
 // create your App component here
+
+import React,{useEffect,useState} from "react"
+
+function App(){
+    const [src,fetchedSrc] = useState("")
+    useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then((r) => r.json())
+    .then((d) => fetchedSrc(d.message))
+    },[]
+    )
+    
+    return(
+    src === "" ? <p>Loading...</p> :
+     <img src={src} alt="A Random Dog"></img>
+        )
+}
+export default App
